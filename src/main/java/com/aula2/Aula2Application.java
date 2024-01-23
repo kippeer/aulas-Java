7cbb76f01b21d0dfa8a1ba711b3af74aedf7e297
Fabio <fabiot92@gmail.com>
2024-01-23 18:57:04 -0300

    Update Aula2Application.java
    criando operaçao de instanciamento

--- src/main/java/com/aula2/Aula2Application.java
+++ src/main/java/com/aula2/Aula2Application.java
@@ -1,13 +1,30 @@
 package com.aula2;
 
+import com.aula2.domain.Categoria;
+import com.aula2.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Aula2Application {
+public class Aula2Application implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(Aula2Application.class, args);
+
+		SpringApplication.run(Aula2Application.class, args
+		);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
