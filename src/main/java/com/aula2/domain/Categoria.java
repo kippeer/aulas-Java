aa442dd48c80312cc49e441043e175b61aa0823f
Fabio <fabiot92@gmail.com>
2024-01-30 17:44:07 -0300

    att
    att

--- src/main/java/com/aula2/domain/Categoria.java
+++ src/main/java/com/aula2/domain/Categoria.java
@@ -1,6 +1,7 @@
 package com.aula2.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    @JsonManagedReference
     @ManyToMany(mappedBy = "categorias")
     private List<Produto> produtos = new ArrayList<>();
     public Categoria(){
