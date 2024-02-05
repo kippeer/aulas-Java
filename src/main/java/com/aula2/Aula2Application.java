0f89e49953d27fedcba479bb4aa60c29527d2607
Fabio <fabiot92@gmail.com>
2024-02-05 17:26:23 -0300

    cidade e estado repositorio

--- src/main/java/com/aula2/Aula2Application.java
+++ src/main/java/com/aula2/Aula2Application.java
@@ -1,8 +1,12 @@
 package com.aula2;
 
 import com.aula2.domain.Categoria;
+import com.aula2.domain.Cidade;
+import com.aula2.domain.Estado;
 import com.aula2.domain.Produto;
 import com.aula2.repositories.CategoriaRepository;
+import com.aula2.repositories.CidadeRepository;
+import com.aula2.repositories.EstadoRepository;
 import com.aula2.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -17,6 +21,10 @@ public class Aula2Application implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 
@@ -41,8 +49,24 @@ public class Aula2Application implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2,p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null,"Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
+
 
 	}
 }
---
+++ src/main/java/com/aula2/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.aula2.repositories;
+
+import com.aula2.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
